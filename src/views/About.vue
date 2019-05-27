d35eb2f0e31522dcbebccd890240fb7f1ab5efe5
wulibaibao_wulibaibao <wulibaibao_wulibaibao@git.cloud.tencent.com>
2019-05-27 18:22:14 +0800

    add some code

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,22 @@
         <h1>This is an about page</h1>
     </div>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    },
+    created () {
+
+    },
+    mounted () {
+
+    },
+    component : {
+        
+    }
+}
+</script>
